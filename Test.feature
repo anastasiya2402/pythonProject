4567b55074b7b87c30161796d8e49979a2ab09ec
anastasiashabanskaya <shabanskayaa@gmail.com>
2021-03-09 18:54:24 -0500

    HW 3

--- Test.feature
+++ Test.feature
@@ -10,32 +10,39 @@ Feature: Regression for eBay Search Combo box.
   Scenario: Verify that search displays right items
   Given Open eBay.com
   And Search for "dress"
-  And Click the Search button
+  And Push button "Search"
     Then All items are dresses
-#  Then Close the Chrome browser
+
 
   Scenario: Verifying that typing "dress" displays category "Dresses"
     Given Open eBay.com
     And Search for "dress"
-    And Press Enter/Return
+    And Push button "Search"
     Then Category Dresses displayed
+
+    Scenario: Trying to find a dress using filter "Buy It Now"
+    Given Open eBay.com
+    And Search for "dress"
+      And Push button "Search"
+    And Choose filter "Buy It Now"
+      Then Verifying that the dress you want is there
 
    Scenario: Verifying that typing "dress1" puts it to category "Dresses"
      Given Open eBay.com
     And Search for "dress1"
-    And Press Enter/Return
+    And Press Enter/Return in "Search for anything" field
     Then Category Dresses displayed
 
   Scenario: Verifying that typing "dress#" puts it to category "Dresses"
      Given Open eBay.com
     And Search for "dress#"
-    And Click the Search button
+    And Push button "Search"
     Then Category Dresses displayed
 
    Scenario: Verifying autocompletion: typing "dres" puts it to category "Dresses"
      Given Open eBay.com
     And Search for "dres"
-    And Press Enter/Return
+    And Press Enter/Return in "Search for anything" field
     And Stats show large number dresses
     Then There is also a suggestion to search for "dres" instead
 
@@ -47,14 +54,46 @@ Feature: Regression for eBay Search Combo box.
      Scenario: Verifying that combination of special characters (without whitespace) are allowed in Search.(Actual: not allowed)
     Given Open eBay.com
     And Enter some special characters
-    And Press Enter/Return
+    And Press Enter/Return in "Search for anything" field
     Then All categories are displayed
 
   Scenario: Verifying that only a whitespace is allowed in Search (Actual: not allowed)
    Given Open eBay.com
     And Press space key in Search combo box
-    And Press Enter/Return
+    And Press Enter/Return in "Search for anything" field
     Then All categories are displayed
+
+   Scenario: Verifying that capacity of "Search" combo box is 300.
+     Given Open eBay.com
+     And Enter iphone 11
+     And Enter iphone 11
+     And Enter iphone 11
+     And Enter iphone 11
+     And Enter iphone 11
+     And Select/Copy/Paste the result
+    And  And paste 5 more times(Total 315 characters)
+     And Count # of actual elements in the field and compare with 300
+    Then Push button "Search"
+    Then Verify the message "No exact matches found"
+
+    Scenario: Click on Sell
+      Given Open eBay.com
+      And Click "Sell" header element
+
+     Scenario: Click on My eBay
+      Given Open eBay.com
+      And Click "My eBay" header element
+
+     Scenario: Click on Notification
+      Given Open eBay.com
+      And Click "Notification" header element
+
+
+
+
+
+
+
 
 
 
--- steps/definitions.py
+++ steps/definitions.py
@@ -25,6 +25,7 @@ def search_smth(context):
 def search_smth(context):
     search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
     search_inpt.send_keys("dress1")
+    sleep(3)
 
 @step('Search for "dress#"')
 def search_smth(context):
@@ -36,24 +37,24 @@ def search_smth(context):
     search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
     search_inpt.send_keys("dres")
 
-@step('Click the Search button')
-def click_the_search_btn(context):
-    srch_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
-    srch_btn.click()
-    sleep(5) #5 seconds
-
 @step('All items are dresses')
 def verify_search_result(context):
     item=context.browser.find_element_by_xpath("//h3[text()='Women Ladies Long Maxi Dress Boho Holiday Beach Party Cocktail Summer Sundress']")
 
-@step('Close the Chrome browser')
-def close_the_browser(context):
-    context.browser.close()
+@step('Choose filter "{link_name}"')
+def choose_filter(context, link_name):
+    filter=context.browser.find_element_by_xpath(f"//h2[@class='srp-format-tabs-h2' and contains(text(), '{link_name}')]")
+    sleep(3)
+    filter.click()
+    sleep(5)
 
+@step('Verifying that the dress you want is there')
+def verify_dress(context):
+    needed_dress=context.browser.find_element_by_xpath("//li[@class='s-item    '][.//span[text()='Free shipping']][.//span[text()='Free returns']][.//span[text()='Buy 1, get 1 10% off']]//h3")
 
-@step('Push button "Search"')
-def click_the_search_btn(context):
-    srch_btn = context.browser.find_element_by_xpath("//input[@type='submit']")
+@step('Push button "{link_of_the_element}"')
+def click_the_search_btn(context, link_of_the_element):
+    srch_btn = context.browser.find_element_by_xpath(f"//*[contains(@class, 'btn-') and contains(@value, '{link_of_the_element}')]")
     srch_btn.click()
     sleep(5)
 
@@ -61,9 +62,9 @@ def click_the_search_btn(context):
 def verify_all_categories(context):
     image=context.browser.find_element_by_xpath("//h1[text()='All Categories']")
 
-@step('Press Enter/Return')
-def submit_search(context):
-    srch_combo_box = context.browser.find_element_by_xpath("//input[@type='submit']")
+@step('Press Enter/Return in "{name_of_the_link}" field')
+def submit_search(context, name_of_the_link):
+    srch_combo_box = context.browser.find_element_by_xpath(f"//input[contains(@aria-label, '{name_of_the_link}') and @role='combobox']")
     srch_combo_box.send_keys(Keys.RETURN)
     sleep(5)
 
@@ -83,8 +84,54 @@ def search_ver_dresses(context):
 def press_space_key(context):
     space_key_btn=context.browser.find_element_by_xpath("//input[@id='gh-ac']")
     space_key_btn.send_keys(Keys.SPACE)
+    sleep(3)
 
 @step('Enter some special characters')
 def search_smth(context):
     search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
     search_inpt.send_keys("@!#$%^&*()-_=+`~|]}[{'/.>,<?")
+
+@step('Enter iphone 11')
+def enter_text(context):
+    enter_element=context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    enter_element.send_keys("iphone 11")
+
+
+@step('Select/Copy/Paste the result')
+def copy_text(context):
+    element=context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    sleep(1)
+    element.send_keys(Keys.COMMAND + 'a')
+    sleep(1)
+    element.send_keys(Keys.COMMAND + 'c')
+    sleep(1)
+    element.send_keys(Keys.ARROW_RIGHT)
+    element.send_keys(Keys.COMMAND + 'v')
+    sleep(1)
+
+@step('And paste 5 more times(Total 315 characters)')
+def paste_text(context):
+    element=context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    element.send_keys(Keys.COMMAND + 'v')
+    element.send_keys(Keys.COMMAND + 'v')
+    element.send_keys(Keys.COMMAND + 'v')
+    element.send_keys(Keys.COMMAND + 'v')
+    element.send_keys(Keys.COMMAND + 'v')
+    sleep(1)
+
+@step('Count # of actual elements in the field and compare with 300')
+def count_number(context):
+    elem=len(context.browser.find_element_by_xpath("//input[@id='gh-ac']").get_attribute("value"))
+    assert elem==300
+    print(elem)
+
+@step('Verify the message "No exact matches found"')
+def count_number(context):
+ element = context.browser.find_element_by_xpath("//h3[@class='srp-save-null-search__heading' and text()='No exact matches found']")
+
+
+@step('Click "{name_of_the_link}" header element')
+def click_sell(context, name_of_the_link):
+    header_action=context.browser.find_element_by_xpath(f"//*[contains(@class, 'gh-') and contains(text(), '{name_of_the_link}')]")
+    sleep(5)
+    header_action.click()
